2f5cb6e9b771d47f2349d4dbda740f4755f9df32
Nicolas Ferreira <nicolasferreira100@gmail.com>
2021-11-16 15:03:06 -0300

    Project created

---
+++ src/main/java/com/nfsoftwares/mongodb/MongodbspringokApplication.java
@@ -0,0 +1,13 @@
+package com.nfsoftwares.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbspringokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbspringokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nfsoftwares/mongodb/MongodbspringokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfsoftwares.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbspringokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
