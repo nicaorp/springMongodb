ebab30e47509825927517a960f2d060c0d960479
Nicolas Ferreira <nicolasferreira100@gmail.com>
2021-11-17 18:12:09 -0300

    Simple search with query methods

--- src/main/java/com/nfsoftwares/mongodb/config/Instantiation.java
+++ src/main/java/com/nfsoftwares/mongodb/config/Instantiation.java
@@ -42,7 +42,7 @@ public class Instantiation implements CommandLineRunner{
 		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 		
-		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));	
 		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
 		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 		
--- src/main/java/com/nfsoftwares/mongodb/respository/PostRepository.java
+++ src/main/java/com/nfsoftwares/mongodb/respository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.nfsoftwares.mongodb.respository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.nfsoftwares.mongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
