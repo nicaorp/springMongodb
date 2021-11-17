6e22a743576eef5a28f8c94713c7dd8819f0f8bd
Nicolas Ferreira <nicolasferreira100@gmail.com>
2021-11-17 16:29:12 -0300

    Create Post entity with User togheter

--- src/main/java/com/nfsoftwares/mongodb/config/Instantiation.java
+++ src/main/java/com/nfsoftwares/mongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.nfsoftwares.mongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.nfsoftwares.mongodb.domain.Post;
 import com.nfsoftwares.mongodb.domain.User;
+import com.nfsoftwares.mongodb.respository.PostRepository;
 import com.nfsoftwares.mongodb.respository.UserRepository;
 
 @Configuration
@@ -15,16 +19,28 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-	
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+		
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 	}
 
