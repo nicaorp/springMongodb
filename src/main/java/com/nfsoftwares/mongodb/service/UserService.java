26567c03eb996baabbff76e7abb2d3f833246d9b
Nicolas Ferreira <nicolasferreira100@gmail.com>
2021-11-17 10:33:39 -0300

    insert user with Post

--- src/main/java/com/nfsoftwares/mongodb/service/UserService.java
+++ src/main/java/com/nfsoftwares/mongodb/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nfsoftwares.mongodb.domain.User;
+import com.nfsoftwares.mongodb.dto.UserDTO;
 import com.nfsoftwares.mongodb.respository.UserRepository;
 import com.nfsoftwares.mongodb.service.exception.ObjectNotFoundException;
 
@@ -26,5 +27,12 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 
 }
