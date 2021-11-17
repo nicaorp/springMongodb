b7f8f30cfaa9d1ee4b4024d8f9ffb1a9db1574a5
Nicolas Ferreira <nicolasferreira100@gmail.com>
2021-11-17 17:11:20 -0300

    Reference user posts

--- src/main/java/com/nfsoftwares/mongodb/config/Instantiation.java
+++ src/main/java/com/nfsoftwares/mongodb/config/Instantiation.java
@@ -41,10 +41,11 @@ public class Instantiation implements CommandLineRunner{
 		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 		
-		
-
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
+		maria.getPosts().addAll(Arrays.asList(p1, p2));
+		
+		userRepository.save(maria);
 	}
 
 }
