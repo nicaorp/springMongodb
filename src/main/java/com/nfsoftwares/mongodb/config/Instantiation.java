52417064d927ee35b37c5c88f084a35fe95673a5
Nicolas Ferreira <nicolasferreira100@gmail.com>
2021-11-17 16:46:24 -0300

    Author data projecting with DTO

--- src/main/java/com/nfsoftwares/mongodb/config/Instantiation.java
+++ src/main/java/com/nfsoftwares/mongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.nfsoftwares.mongodb.domain.Post;
 import com.nfsoftwares.mongodb.domain.User;
+import com.nfsoftwares.mongodb.dto.AuthorDTO;
 import com.nfsoftwares.mongodb.respository.PostRepository;
 import com.nfsoftwares.mongodb.respository.UserRepository;
 
@@ -35,11 +36,13 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+		
+		
+
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 	}
