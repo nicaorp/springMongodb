c00ab83830cccea916c4896b0c6a1da13bc01b69
Nicolas Ferreira <nicolasferreira100@gmail.com>
2021-11-17 10:00:28 -0300

    Finding user by id

--- src/main/java/com/nfsoftwares/mongodb/service/UserService.java
+++ src/main/java/com/nfsoftwares/mongodb/service/UserService.java
@@ -1,12 +1,14 @@
 package com.nfsoftwares.mongodb.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nfsoftwares.mongodb.domain.User;
 import com.nfsoftwares.mongodb.respository.UserRepository;
+import com.nfsoftwares.mongodb.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,11 @@ public class UserService {
 		return repo.findAll();
 		
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
+	
 
 }
