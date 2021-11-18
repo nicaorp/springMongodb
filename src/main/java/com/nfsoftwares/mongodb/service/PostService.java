10d4cf0a610087218e286691aeda9038c7dff0dd
Nicolas Ferreira <nicolasferreira100@gmail.com>
2021-11-18 10:19:24 -0300

    Search with many criteria

--- src/main/java/com/nfsoftwares/mongodb/service/PostService.java
+++ src/main/java/com/nfsoftwares/mongodb/service/PostService.java
@@ -1,5 +1,6 @@
 package com.nfsoftwares.mongodb.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +27,10 @@ public class PostService {
 		return repo.findByTitle(text);
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate ) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 *1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 
 
 }
